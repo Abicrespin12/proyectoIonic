70980a61c4dbf6e39c7c30a3fc728d6dbbfd6197
Abicrespin12 <crespinabii@gmail.com>
2022-08-26 01:00:41 -0300

    ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'curriculum',
+    loadChildren: () => import('./pages/curriculum/curriculum.module').then( m => m.CurriculumPageModule)
+  },
+  {
+    path: 'autor',
+    loadChildren: () => import('./pages/autor/autor.module').then( m => m.AutorPageModule)
+  },
+  {
+    path: 'bibloteca',
+    loadChildren: () => import('./pages/bibloteca/bibloteca.module').then( m => m.BiblotecaPageModule)
   },
 ];
 
